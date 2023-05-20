51c1955eb42cab58a518027d9e45204dd0f4b7b7
VSS30 <vini.baxista@outlook.com>
2023-05-20 19:13:47 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+
+
+class Config:
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'baseico.db')
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SECRET_KEY = 'wrgwerthgq343357tyh~];~WDQ'
+    # SECURITY_PASSWORD_SALT = 'fgdhdsg55'
+    # SECURITY_PASSWORD_HASH = 'sha512_crypt'
+    # UPLOAD_FOLDER = 'static'
+    # 34.151.228.9
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+       
